4df5287974fdfb0df2d4278494f324e8aeebde0f
KavindaSandamal <info.kavindasandamal@gmail.com>
2024-06-11 20:14:07 +0530

    modify Pdf view page

--- frontend/src/components/PDFViewer/PDFViewer.js
+++ frontend/src/components/PDFViewer/PDFViewer.js
@@ -3,6 +3,8 @@ import axios from './axiosConfig';
 import { Document, Page, pdfjs } from 'react-pdf';
 import { useParams } from 'react-router-dom';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import '../../App.css';
+
 
 // Set the workerSrc globally for PDF.js
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
@@ -42,24 +44,22 @@ const PDFViewer = () => {
   };
 
   return (
-    <div>
-      {error && <div>Error: {error}</div>}
-      {pdfUrl && (
-        <Document
-          file={pdfUrl}
-          onLoadSuccess={onDocumentLoadSuccess}
-        >
-          {Array.from(
-            new Array(numPages),
-            (el, index) => (
-              <Page
-                key={`page_${index + 1}`}
-                pageNumber={index + 1}
-              />
-            ),
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="max-w-screen-md w-full text-center bg-white p-8 rounded-lg shadow-md overflow-hidden">
+        <div className="overflow-y-auto h-96 bg-red-200"> {/* Change background color here */}
+          {error && <div className="text-red-500">Error: {error}</div>}
+          {pdfUrl && (
+            <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
+              {Array.from(
+                new Array(numPages),
+                (el, index) => (
+                  <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+                )
+              )}
+            </Document>
           )}
-        </Document>
-      )}
+        </div>
+      </div>
     </div>
   );
 };

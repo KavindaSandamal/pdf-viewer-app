f289c42014dd7ca7c5077dee938caab36e4ff1d2
KavindaSandamal <info.kavindasandamal@gmail.com>
2024-06-13 10:03:50 +0530

    Update PDFViewer scrolling

--- frontend/src/components/PDFViewer/PDFViewer.js
+++ frontend/src/components/PDFViewer/PDFViewer.js
@@ -15,10 +15,10 @@ const PDFViewer = () => {
   const { id } = useParams();
   const [pdfUrl, setPdfUrl] = useState(null);
   const [numPages, setNumPages] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1); // New state for the current page
+  const [currentPage, setCurrentPage] = useState(1);
   const [pdfZoom, setPdfZoom] = useState(1);
   const [error, setError] = useState(null);
-  const [fitToPage, setFitToPage] = useState(false); // New state for fitting PDF to page
+  const [fitToPage, setFitToPage] = useState(false);
 
   const PDF_INITIAL_SCALE = isMobile ? 0.6 : 1;
   const PDF_MAX_ZOOM = PDF_INITIAL_SCALE + 1;
@@ -39,21 +39,19 @@ const PDFViewer = () => {
 
     fetchPdf();
 
-    // Cleanup blob URL on unmount
     return () => {
       if (pdfUrl) {
         URL.revokeObjectURL(pdfUrl);
       }
     };
   }, [id]);
-  
 
   const onDocumentLoadSuccess = ({ numPages }) => {
     setNumPages(numPages);
   };
 
   const handleZoomChange = (offset) => {
-    setPdfZoom(prevZoom => prevZoom + offset);
+    setPdfZoom(prevZoom => Math.min(Math.max(prevZoom + offset, PDF_INITIAL_SCALE), PDF_MAX_ZOOM));
   };
 
   const handleFitToPage = () => {
@@ -62,27 +60,15 @@ const PDFViewer = () => {
 
   const goToNextPage = () => {
     if (currentPage < numPages) {
-      setCurrentPage(currentPage + 1);
-      scrollToPage(currentPage + 1);
+      setCurrentPage(prevPage => prevPage + 1);
     }
   };
-  
+
   const goToPrevPage = () => {
     if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-      scrollToPage(currentPage - 1);
+      setCurrentPage(prevPage => prevPage - 1);
     }
   };
-  
-  const scrollToPage = (pageNumber) => {
-    const pageElement = document.getElementById(`page_${pageNumber}`);
-    if (pageElement) {
-      pageElement.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
-    }
-  };
-  
-  
-  
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
@@ -92,37 +78,38 @@ const PDFViewer = () => {
     }
   };
 
+  useEffect(() => {
+    const pageElement = document.getElementById(`page_${currentPage}`);
+    if (pageElement) {
+      pageElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [currentPage]);
+
   return (
     <div className="flex flex-col items-center w-full h-screen bg-stone-400 p-3">
-      <div className="bg-stone-400 overflow-auto w-fit">
+      <div className="bg-stone-400 overflow-auto w-fit h-full">
         {error && <div className="text-red-500">Error: {error}</div>}
-        {currentPage && (
-        <Document
-          file={pdfUrl}
-          onLoadSuccess={onDocumentLoadSuccess}
-          options={{ scrollMode: 'vertical' }}
-        >
-          {Array.from(new Array(numPages - currentPage + 1), (el, index) => (
-            <div key={`page_${index + 1}`} className="page-container mb-5 last:mb-0 bg-stone-400 ">
-              <Page key={`page_${currentPage + index}`} pageNumber={currentPage + index} scale={fitToPage ? 'auto' : pdfZoom * 1.5} />
-            </div>
-          ))}
-        </Document>
-      )}
+        {pdfUrl && (
+          <Document
+            file={pdfUrl}
+            onLoadSuccess={onDocumentLoadSuccess}
+          >
+            {Array.from(new Array(numPages), (el, index) => (
+              <div key={`page_${index + 1}`} id={`page_${index + 1}`} className="page-container mb-5 last:mb-0 bg-stone-400">
+                <Page pageNumber={index + 1} scale={fitToPage ? 'auto' : pdfZoom * 1.5} />
+              </div>
+            ))}
+          </Document>
+        )}
       </div>
       <div className="fixed left-0 right-0 bottom-3 bg-stone-700 w-fit m-auto p-2 rounded-lg opacity-[0.9]">
         <div className="flex items-center">
           <button
             disabled={currentPage === 1}
-            className={`p-2 rounded-lg `}
+            className={`p-2 rounded-lg ${currentPage === 1 && 'text-slate-400'}`}
             onClick={goToPrevPage}
           >
-            <span
-              className={`text-md ${currentPage === 1 && "text-slate-400"
-                } text-white`}
-            >
-              Back
-            </span>
+            <span className="text-md text-white">Back</span>
           </button>
 
           <div>
@@ -133,21 +120,15 @@ const PDFViewer = () => {
 
           <button
             disabled={currentPage === numPages}
-            className={`p-2 rounded-lg `}
+            className={`p-2 rounded-lg ${currentPage === numPages && 'text-slate-400'}`}
             onClick={goToNextPage}
           >
-            <span
-              className={`text-md text-white ${currentPage === numPages && "text-slate-400"
-                }`}
-            >
-              Next
-            </span>
+            <span className="text-md text-white">Next</span>
           </button>
 
           <button
             type="button"
-            className={`text-white text-[2rem] px-3 ${pdfZoom <= PDF_INITIAL_SCALE ? "text-[silver]" : "text-white"
-              }`}
+            className={`text-white text-[2rem] px-3 ${pdfZoom <= PDF_INITIAL_SCALE ? 'text-[silver]' : 'text-white'}`}
             onClick={() => handleZoomChange(-0.2)}
           >
             <Icon icon="material-symbols:zoom-out" />
@@ -155,8 +136,7 @@ const PDFViewer = () => {
 
           <button
             type="button"
-            className={`text-[2rem] px-3 ${pdfZoom >= PDF_MAX_ZOOM ? "text-[silver]" : "text-white"
-              }`}
+            className={`text-[2rem] px-3 ${pdfZoom >= PDF_MAX_ZOOM ? 'text-[silver]' : 'text-white'}`}
             onClick={() => handleZoomChange(0.2)}
           >
             <Icon icon="material-symbols:zoom-in" />
